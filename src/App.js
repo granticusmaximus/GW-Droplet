59cc6c65ea3806f614d05921efb3c46f9a98e6b3
Grant Watson <gwatson117@gmail.com>
2020-01-10 13:03:33 -0700

    major update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-import HomePage from './Views/Home';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import * as ROUTES from './constants/routes';
-import PortolioPage from './Views/Portfolio';
-import BlogPage from './Views/Blog';
-import ServicePage from './Views/Services';
-import ExperiencePage from './Views/Experience';
-import TopNav from './components/Navigation/TopNav';
-import StickyFooter from 'react-sticky-footer';
-import Footer from './components/Footer/Footer';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="http://grantwatson.app">
-        Grant Watson
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
+import HomePage from "./Views/Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import PortolioPage from "./Views/Portfolio";
+import BlogPage from "./Views/Blog";
+import ServicePage from "./Views/Services";
+import ExperiencePage from "./Views/Experience";
+import TopNav from "./components/Navigation/TopNav";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
     <Router>
       <TopNav />
       <div className="container-body">
-
-
         <Box my={5}>
           <div>
             <Route exact path={ROUTES.HOME} component={HomePage} />
@@ -42,10 +26,7 @@ export default function App() {
             <Route exact path={ROUTES.EXPERIENCE} component={ExperiencePage} />
           </div>
         </Box>
-
       </div>
-      
-      
 
       <Footer />
     </Router>
--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,52 +1,56 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import React, { Component } from "react";
+import SkillBar from "react-skillbars";
 
-const HomePage = () => (
-  <div className="container">
-    <div className="row">
-      <div className="col-md-4">
-        <div className="card card-1">
-          <h4>Georgia Native | Colorado Transplant</h4>
-          <p>I got my degree from Middle Georgia State University in 2018, and moved to Colorado with my wife in 2019.</p>
+class HomePage extends Component {
+  render() {
+    const skills = [
+      { type: "C#", level: 85 },
+      { type: "ASP.NET", level: 85 },
+      { type: "ReactJS", level: 88 },
+      { type: "Java", level: 75 },
+      { type: "Python", level: 70 },
+      { type: "PHP", level: 80 },
+      { type: "Firebase", level: 56 },
+      { type: "MySQL", level: 65 },
+      { type: "SQL Server", level: 68 },
+      { type: "Javascript", level: 75 }
+    ];
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card card-1">
+              <h4>Georgia Native | Colorado Transplant</h4>
+              <p>
+                I got my degree from Middle Georgia State University in 2018,
+                and moved to Colorado with my wife in 2019.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card card-2">
+              <h4>Top Notch Dev</h4>
+              <p>
+                As per the arena, I love learning the newest languages, trends,
+                and features that the provide the Internet it's glory.
+              </p>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card card-3">
+              <h5 className="mb-3 text-black">
+                <strong>Skills</strong>
+              </h5>
+              <SkillBar skills={skills} />
+            </div>
+          </div>
         </div>
       </div>
-      <div className="col-md-4">
-        <div className="card card-2">
-          <h4>Top Notch Dev</h4>
-          <p>As per the arena, I love learning the newest languages, trends, and features that the provide the Internet it's glory.</p>
-        </div>
-      </div>
-      <div className="col-md-4">
-        <div className="card card-3">
-          <h3>Current Tech Stacks</h3>
-          <p>Every developer has their favorite tech stack, or tools attached to their belt. I am no different</p>
-          <ul>
-            <li>C#</li>
-            <li>ASP.NET</li>
-            <li>ReactJS</li>
-            <li>Firebase</li>
-            <li>MeteorJS</li>
-            <li>NextJS</li>
-          </ul>
-        </div>
-      </div>
-
-      <hr />
-      <Typography variant="h4" component="h1" gutterBottom>
-        Welcome! I am Grant Watson
-      </Typography>
-    </div>
-
-
-
-
-
-
-
-
-
-
-  </div>
-);
+    );
+  }
+}
 
 export default HomePage;
