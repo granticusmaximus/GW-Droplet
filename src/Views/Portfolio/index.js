909454a34e498a828521561b5b91d0e19f1109c9
Grant Watson <gwatson117@gmail.com>
2020-01-13 08:07:12 -0700

    Update to Portfolio Page

--- src/Views/Portfolio/index.js
+++ src/Views/Portfolio/index.js
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
 import Typography from '@material-ui/core/Typography';
+import Fade from 'react-reveal/Fade';
+import data from './data';
+import Project from './Project';
 
 class PortolioPage extends Component {
+  state = {}
   render() {
     return (
       <div className="container">
-        <Typography variant="h4" component="h1" gutterBottom>
-          Portfolio Page
-        </Typography>
+
+        <div>
+          <h1 className='heading'>
+            <Fade bottom cascade>
+              <Typography variant="h4" component="h1" gutterBottom>
+                My Current Work
+              </Typography>
+            </Fade>
+          </h1>
+          <center>
+            <div className='work-content'>
+              {data.projects.map((project) => (
+                <Project key={project.id}
+                  title={project.title}
+                  service={project.service}
+                  imageSrc={project.imageSrc}
+                  url={project.url}
+                ></Project>
+              ))}
+            </div>
+          </center>
+        </div>
       </div>
+
     );
   }
 }
