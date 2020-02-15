5bb761f329d77d6e0281156e9c55ac66115fc435
Grant Watson <gwatson117@gmail.com>
2020-02-15 16:11:03 -0700

    Addition of Analytics

--- src/index.js
+++ src/index.js
@@ -5,11 +5,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
 /*const store = createStore(reducers, {}, applyMiddleware(reduxThunk));*/
 
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 ReactDOM.render(
-  <App />,
+  <Router history={history}>
+    <App />
+  </Router>,
   document.getElementById("root")
 );
 serviceWorker.register();
